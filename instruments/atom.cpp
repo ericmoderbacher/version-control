6dc0f33f1874df7136a51da197b0ba7c3f57f21f
ericmoderbacher <eric@moderbacher.com>
2024-01-04 21:50:15 -0800

    getting some starting ideas down on electronic paper.

---
+++ instruments/atom.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Eric Moderbacher on 1/4/24.
+//
+
+//atom as in before we knew they wer divisible
+
+//I think it would be interesting if the way this specific instance of
+
+template<typename cordinateType>
+class Atom
+{
+    private
+    cordinateType x, y, z;
+    //function pointer to the way we figure out our location
+    void (*locationFunction)(cordinateType, cordinateType, cordinateType);
+
+
+    public
+
+
+
+    //function pointer to the way we figure out our location
+
+
+};
